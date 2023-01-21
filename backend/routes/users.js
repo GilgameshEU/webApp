657b1babc39cb581b20b038865e97189eb458ac1
GilgameshEU <d1vergence@yandex.ru>
2023-01-21 16:14:00 +0400

    21.01.2023

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("../config/database");
 // another routes also appear here
 // this script to fetch data from MySQL databse table
+
 router.get("/user-list", function (req, res, next) {
   const sql = "SELECT * FROM users";
   db.query(sql, function (err, data, fields) {
@@ -25,9 +26,10 @@ router.get("/delete/:id", function (request, res, next) {
 });
 
 //group del
-router.delete("/groupDelete", function (request, res, next) {
+router.post("/groupDelete", function (request, res, next) {
   var groupIds = request.body.shouldDelete;
-  const sql = `DELETE FROM users WHERE id IN ("${groupIds.join(",")}") `;
+  console.log(request.body.shouldDelete);
+  const sql = `DELETE FROM users WHERE id IN "${groupIds.join(",")}" `;
   db.query(sql, function (err, data) {
     if (err) {
       throw err;
